27fe8cfe8e2e99ab1e0c5120886da4caaf2341e6
samuelfsd <samuel.estudo778@gmail.com>
2023-11-02 11:36:29 -0300

    jest tests basics

--- verifyNumbers.js
+++ verifyNumbers.js
@@ -1,8 +1,10 @@
 const numbers = (arr) => {
   for (let index = 0; index < arr.length; index += 1) {
     if (typeof arr[index] !== "number") return false;
-    return true;
   }
+  return true;
 };
+
+console.log(numbers([1, "2", 3]));
 
 module.exports = numbers;
--- verifyNumbers.test.js
+++ verifyNumbers.test.js
@@ -1,7 +1,19 @@
 const numbers = require("./verifyNumbers");
 
-describe("Testa a função numbers", () => {
+describe("Testa 1", () => {
   it("A função recebe [1,2,3,4,5] e retorna true", () => {
     expect(true).toEqual(numbers([1, 2, 3, 4, 5]));
+  });
+});
+
+describe("Testa 2", () => {
+  it("A função recebe [1,2,3,4,5] e retorna true", () => {
+    expect(false).toEqual(numbers([1,'2', 3, 4, 5]));
+  });
+});
+
+describe("Testa 3", () => {
+  it("A função recebe [1,2,3,4,5] e retorna true", () => {
+    expect(false).toEqual(numbers([" "]));
   });
 });
